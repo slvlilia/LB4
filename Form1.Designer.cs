278b07cdccd321b741f15650ac4f1789b14d28f8
slvlilia <118120852+slvlilia@users.noreply.github.com>
2023-10-05 01:00:00 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace WindowsFormLB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelResult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Coral;
+            this.button1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(49, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Обчислити ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.BackColor = System.Drawing.SystemColors.Info;
+            this.textBoxX.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.textBoxX.Location = new System.Drawing.Point(49, 78);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(100, 22);
+            this.textBoxX.TabIndex = 1;
+            this.textBoxX.Text = "Ввести X";
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.BackColor = System.Drawing.SystemColors.Info;
+            this.textBoxY.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.textBoxY.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.textBoxY.Location = new System.Drawing.Point(49, 127);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(100, 22);
+            this.textBoxY.TabIndex = 2;
+            this.textBoxY.Text = "Ввести Y";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.pictureBox1.Location = new System.Drawing.Point(459, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(195, 129);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.BackColor = System.Drawing.SystemColors.Info;
+            this.labelResult.Location = new System.Drawing.Point(510, 231);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(44, 16);
+            this.labelResult.TabIndex = 4;
+            this.labelResult.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormLB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+                // Отримуємо значення координат X та Y з TextBox.
+                if (int.TryParse(textBoxX.Text, out int x) && int.TryParse(textBoxY.Text, out int y))
+                {
+                    // Визначаємо четверть системи координат.
+                    string quarter = "";
+                    if (x > 0 && y > 0)
+                        quarter = "I";
+                    else if (x < 0 && y > 0)
+                        quarter = "II";
+                    else if (x < 0 && y < 0)
+                        quarter = "III";
+                    else if (x > 0 && y < 0)
+                        quarter = "IV";
+
+                    // Відображаємо результат на PictureBox.
+                    Graphics graphics = pictureBox1.CreateGraphics();
+                    graphics.Clear(Color.White);
+                    Brush brush = new SolidBrush(Color.Red);
+                    int radius = 5;
+                    int centerX = pictureBox1.Width / 2;
+                    int centerY = pictureBox1.Height / 2;
+                    graphics.FillEllipse(brush, centerX + x - radius, centerY - y - radius, 2 * radius, 2 * radius);
+
+                    // Відображаємо результат (четверть) в Label.
+                    labelResult.Text = "Четверть: " + quarter;
+                }
+                else
+                {
+                    MessageBox.Show("Некоректні дані. Введіть цілі числа для координат X та Y.");
+                }
+            }
+
+        }
+    }
+
